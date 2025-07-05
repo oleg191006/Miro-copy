4ffd16ce4bfe8d8b8b756a5c5110a0bbca6bb935
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-05 21:52:51 +0300

    feat: implement react router

---
+++ src/features/board/board.page.tsx
@@ -0,0 +1,9 @@
+import { ROUTES, type PathParams } from "@/shared/model/routes";
+import { useParams } from "react-router-dom";
+
+function BoardPage() {
+  const params = useParams<PathParams[typeof ROUTES.BOARD]>();
+  return <div>Board page {params.boardId}</div>;
+}
+
+export const Component = BoardPage;
