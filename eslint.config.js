fe1e542c6a0f140f5ff7fd09ba20d18647300264
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-05 21:09:43 +0300

    feat: configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import { eslintBoundariesConfig } from './eslint.boundaries.js'
+
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -20,4 +22,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  eslintBoundariesConfig
 ])
