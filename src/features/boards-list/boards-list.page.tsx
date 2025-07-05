5cfaf49a43f025e3a3a3267637cb2f085b7604c4
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-06 01:05:44 +0300

    feat: add basic CRUD, mocks data and connect tanstack query

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,13 +1,43 @@
+import { rqClient } from "@/shared/api/instance";
 import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
+import { useQueryClient } from "@tanstack/react-query";
 import { href, Link } from "react-router-dom";
 
 function BoardsListPage() {
+  const queryClient = useQueryClient();
   console.log(CONFIG.API_BASE_URL);
+  const boardQuery = rqClient.useQuery("get", "/boards");
+  // const createBoardMutation = rqClient.useMutation("post", "/boards");
+  const deleteBoardMutation = rqClient.useMutation(
+    "delete",
+    "/boards/{boardId}",
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries({ queryKey: ["get", "/boards"] });
+      },
+    }
+  );
   return (
     <div>
       <h1>Boards list {CONFIG.API_BASE_URL}</h1>
-      <Link to={href(ROUTES.BOARD, { boardId: "1" })}></Link>
+      {boardQuery.data?.map((board) => (
+        <div key={board.id}>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+            {board.name}
+          </Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() =>
+              deleteBoardMutation.mutate({
+                params: { path: { boardId: board.id } },
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/shared/api/schema/index.ts
@@ -0,0 +1,6 @@
+import type { components, paths } from "./generated";
+
+export type ApiPaths = paths;
+export type ApiSchemas = components["schemas"];
+
+export type R = ApiSchemas["CreateBoard"];
