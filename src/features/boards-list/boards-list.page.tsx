c37728f65fec7ab74bf06b3171ca4a0f36866689
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-06 08:50:01 +0300

    feat: connect tailwindcss and shadcn

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,6 +1,8 @@
 import { rqClient } from "@/shared/api/instance";
 import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
+import { Button } from "@/shared/ui/kit/button";
+import { Card, CardFooter, CardHeader } from "@/shared/ui/kit/card";
 import { useQueryClient } from "@tanstack/react-query";
 import { href, Link } from "react-router-dom";
 
@@ -19,25 +21,34 @@ function BoardsListPage() {
     }
   );
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <h1>Boards list {CONFIG.API_BASE_URL}</h1>
-      {boardQuery.data?.map((board) => (
-        <div key={board.id}>
-          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
-            {board.name}
-          </Link>
-          <button
-            disabled={deleteBoardMutation.isPending}
-            onClick={() =>
-              deleteBoardMutation.mutate({
-                params: { path: { boardId: board.id } },
-              })
-            }
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {boardQuery.data?.map((board) => (
+          <Card key={board.id}>
+            <CardHeader>
+              <Button asChild variant="link">
+                <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+                  {board.name}
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardFooter>
+              <Button
+                variant="destructive"
+                disabled={deleteBoardMutation.isPending}
+                onClick={() =>
+                  deleteBoardMutation.mutate({
+                    params: { path: { boardId: board.id } },
+                  })
+                }
+              >
+                Delete
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
